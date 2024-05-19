056fb4dc52c4ddd13fde081b8831f3ba2dab7ee3
ririkuchan <ririkuchan@gmail.com>
2024-05-19 17:05:00 +0900

    W03 Assignment

--- scripts/place.js
+++ scripts/place.js
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('wind-chill').textContent = placeData.windChill;
 
     function calculateWindChill(temperature, windSpeed) {
-        if (temperature <= 10 && windSpeed > 4.8) {
+        if (temperature <= 30 && windSpeed > 1) {
             return (13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16)).toFixed(1);
         } else {
             return 'N/A';
