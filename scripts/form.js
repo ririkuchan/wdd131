e3910239a0cae3223d1721832f643700e25c80b0
ririkuchan <ririkuchan@gmail.com>
2024-06-04 13:16:54 +0900

    W05 Assignment: Product Review Form

--- scripts/form.js
+++ scripts/form.js
@@ -4,28 +4,33 @@ const products = [
     { id: "fs-1987", name: "time circuits", avgRating: 3.5 },
     { id: "ac-2000", name: "low voltage reactor", avgRating: 3.9 },
     { id: "jj-1969", name: "warp equalizer", avgRating: 5.0 }
-  ];
-  
+];
+
 document.addEventListener('DOMContentLoaded', () => {
-      const productNameSelect = document.getElementById('productName');
-      products.forEach(product => {
-          const option = document.createElement('option');
-          option.value = product.name;
-          option.textContent = product.name;
-          productNameSelect.appendChild(option);
-      });
-  
-      if (localStorage.getItem('reviewCounter') === null) {
-          localStorage.setItem('reviewCounter', '0');
-      }
-});
-  
-window.addEventListener('load', () => {
-      const reviewCounter = localStorage.getItem('reviewCounter');
-      localStorage.setItem('reviewCounter', (parseInt(reviewCounter) + 1).toString());
+    const productNameSelect = document.getElementById('productName');
+    products.forEach(product => {
+        const option = document.createElement('option');
+        option.value = product.name;
+        option.textContent = product.name;
+        productNameSelect.appendChild(option);
+    });
+
+    if (localStorage.getItem('reviewCounter') === null) {
+        localStorage.setItem('reviewCounter', '0');
+    }
+
+    const yearSpan = document.getElementById('currentYear');
+    if (yearSpan) {
+        yearSpan.textContent = new Date().getFullYear();
+    }
+
+    const lastModifiedSpan = document.getElementById('lastModified');
+    if (lastModifiedSpan) {
+        lastModifiedSpan.textContent = document.lastModified;
+    }
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    const year = new Date().getFullYear();
-    const lastModified = document.lastModified;
+window.addEventListener('load', () => {
+    const reviewCounter = localStorage.getItem('reviewCounter');
+    localStorage.setItem('reviewCounter', (parseInt(reviewCounter) + 1).toString());
 });
