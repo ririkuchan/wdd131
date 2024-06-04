104884eb44c29d0b19352aab631699a257199358
ririkuchan <ririkuchan@gmail.com>
2024-06-04 13:06:20 +0900

    W05 Assignment: Product Review Form

---
+++ scripts/form.js
@@ -0,0 +1,31 @@
+const products = [
+    { id: "fc-1888", name: "flux capacitor", avgRating: 4.5 },
+    { id: "fc-2050", name: "power laces", avgRating: 4.7 },
+    { id: "fs-1987", name: "time circuits", avgRating: 3.5 },
+    { id: "ac-2000", name: "low voltage reactor", avgRating: 3.9 },
+    { id: "jj-1969", name: "warp equalizer", avgRating: 5.0 }
+  ];
+  
+document.addEventListener('DOMContentLoaded', () => {
+      const productNameSelect = document.getElementById('productName');
+      products.forEach(product => {
+          const option = document.createElement('option');
+          option.value = product.name;
+          option.textContent = product.name;
+          productNameSelect.appendChild(option);
+      });
+  
+      if (localStorage.getItem('reviewCounter') === null) {
+          localStorage.setItem('reviewCounter', '0');
+      }
+});
+  
+window.addEventListener('load', () => {
+      const reviewCounter = localStorage.getItem('reviewCounter');
+      localStorage.setItem('reviewCounter', (parseInt(reviewCounter) + 1).toString());
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const year = new Date().getFullYear();
+    const lastModified = document.lastModified;
+});
